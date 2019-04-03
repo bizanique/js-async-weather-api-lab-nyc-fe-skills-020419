479d2197f6bd86963088de92b9e861e834eac2a0
shaniqua simpson <niquacharisdesigns@gmail.com>
2019-04-03 23:59:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,10 @@ function displayCurrentWeather(json) {
 
 
 function fetchFiveDayForecast(city) {
-  //fetch five day forecast data based on city
+  //fetch five day forecast data based on city 
+  fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + API_KEY + "&units=imperial")
+  .then( (data) => {return data.json()} ) 
+  .then( (json) => {displayFiveDayForecast(json)} )
 }
 
 function displayFiveDayForecast(json) {
